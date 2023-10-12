e84eb3ee685e12bc3de876f1a980efc8ec7be0c0
Ericw <eweissm1@asu.edu>
2023-10-12 12:40:27 -0700

    commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ h = 15  # mm
 FixedArmLength = 70  # mm
 
 # target position and orientation
-PlatformTranslation = np.transpose(np.array([0, 0, 90]))  # [x_p, y_p, z_p]
+PlatformTranslation = np.transpose(np.array([0, 0, 50]))  # [x_p, y_p, z_p]
 psi_deg = 0
 theta_deg = 0
 phi_deg = 0
@@ -22,11 +22,22 @@ BaseToPlatformRotationMatrix = np.array([[np.cos(psi)*np.cos(theta), -np.sin(psi
                                 [np.sin(psi)*np.cos(theta), np.cos(psi)*np.cos(phi)+np.sin(psi)*np.sin(theta)*np.sin(phi), -np.cos(psi)*np.sin(phi)+np.sin(psi)*np.sin(theta)*np.cos(phi)],
                                 [-np.sin(theta), np.cos(theta)*np.sin(phi), np.cos(theta)*np.cos(phi)]])
 
-# Calculate the arm lengths
-l_i = np.zeros((6,3))
+
+
+alpha = np.zeros(6)
 for i in range(6):
       b_i = np.transpose(np.array([BaseRadius*np.cos(i*np.pi/3), BaseRadius*np.sin(i*np.pi/3), 0]))  # Base anchor points [x_b, y_b, z_b]
       p_i = np.transpose(np.array([platformRadius*np.cos(i*np.pi/3), platformRadius*np.sin(i*np.pi/3), 0])) # platform anchor points [x_p, y_p, z_p]
-      l_i[i] = PlatformTranslation + np.matmul(BaseToPlatformRotationMatrix, p_i) - b_i
 
-print(l_i)
+      # Calculate the arm lengths
+      l_i = PlatformTranslation + np.matmul(BaseToPlatformRotationMatrix, p_i) - b_i
+
+      beta_i = i*np.pi/3
+
+      e_i = 2*h*l_i[2]
+      f_i = 2*h*(np.cos(beta_i)*l_i[0]+np.sin(beta_i)*l_i[1])
+      g_i = np.linalg.norm(l_i)**2-(FixedArmLength**2-h**2)
+
+      alpha[i] = np.arcsin(g_i/np.sqrt(e_i**2+f_i**2)) - np.arctan2(f_i, e_i)
+
+print(alpha*180/np.pi)
