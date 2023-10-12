c8d3fe592580e78da149971f4f1e3e1a9f986dd6
Ericw <eweissm1@asu.edu>
2023-10-12 11:25:31 -0700

    commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # define constants for  the platform
-BaseDiameter = 105  # mm
-platformDiameter = 45  # mm
+BaseRadius = 105/2  # mm
+platformRadius = 45/2  # mm
 h = 15  # mm
 
 # target position and orientation
@@ -19,4 +19,18 @@ phi = phi_deg*(np.pi/180)
 # calculate full rotation matrix
 BaseToPlatformRotationMatrix = [[np.cos(psi)*np.cos(theta), -np.sin(psi)*np.cos(phi)+np.cos(psi)*np.sin(theta)*np.sin(phi), np.sin(psi)*np.sin(phi)+np.cos(psi)*np.sin(theta)*np.cos(phi)],
                                 [np.sin(psi)*np.cos(theta), np.cos(psi)*np.cos(phi)+np.sin(psi)*np.sin(theta)*np.sin(phi), -np.cos(psi)*np.sin(phi)+np.sin(psi)*np.sin(theta)*np.cos(phi)],
-                                [-np.cos(theta), np.cos(theta)*np.sin(phi), np.cos(theta)*np.cos(phi)]]
+                                [-np.sin(theta), np.cos(theta)*np.sin(phi), np.cos(theta)*np.cos(phi)]]
+
+B_i = [[BaseRadius*np.cos(0*np.pi()/3), BaseRadius*np.sin(0*np.pi()/3), 0],
+      [BaseRadius*np.cos(1*np.pi()/3), BaseRadius*np.sin(1*np.pi()/3), 0],
+      [BaseRadius*np.cos(2*np.pi()/3), BaseRadius*np.sin(2*np.pi()/3), 0],
+      [BaseRadius*np.cos(3*np.pi()/3), BaseRadius*np.sin(3*np.pi()/3), 0],
+      [BaseRadius*np.cos(4*np.pi()/3), BaseRadius*np.sin(4*np.pi()/3), 0],
+      [BaseRadius*np.cos(5*np.pi()/3), BaseRadius*np.sin(5*np.pi()/3), 0]]  # Base anchor points [x_b, y_b, z_b]
+
+P_i = [[platformRadius*np.cos(0*np.pi()/3), platformRadius*np.sin(0*np.pi()/3), 0],
+      [platformRadius*np.cos(1*np.pi()/3), platformRadius*np.sin(1*np.pi()/3), 0],
+      [platformRadius*np.cos(2*np.pi()/3), platformRadius*np.sin(2*np.pi()/3), 0],
+      [platformRadius*np.cos(3*np.pi()/3), platformRadius*np.sin(3*np.pi()/3), 0],
+      [platformRadius*np.cos(4*np.pi()/3), platformRadius*np.sin(4*np.pi()/3), 0],
+      [platformRadius*np.cos(5*np.pi()/3), platformRadius*np.sin(5*np.pi()/3), 0]]  # platform anchor points [x_p, y_p, z_p]
